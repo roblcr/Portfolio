16c431bd6e38551a16d7dc4f87f4b921f5c495bf
roblcr <lecullier.robin@gmail.com>
2021-12-16 09:12:18 +0100

    initialisation du repo et creation du thème

---
+++ bin/_configuration.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MYSQL_USER="robin"
+MYSQL_PASSWORD="momota"
+MYSQL_HOST="localhost"
+WORDPRESS_BDD="portfolio"
+
+WORDPRESS_URL="http://localhost/Portfolio/public"
+
+WORDPRESS_SITE_NAME="Portfolio"
+WORDPRESS_ADMIN_NAME="admin"
+WORDPRESS_ADMIN_PASSWORD="admin"
+WORDPRESS_ADMIN_EMAIL="admin@mail.com"
+
+# optionnal configurations
+WORDPRESS_FOLDER="wp"
+WORDPRESS_CONTENT_FOLDER="content"
+WORDPRESS_TABLE_PREFIX="wp_portfolio"
